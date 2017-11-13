fc3b4d4507c2734883de5685473c7e82a779b646
Richard Hill <richardhill101@gmail.com>
2017-11-13 19:24:31 +0000

    First commit

---
+++ Events/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Events
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void OnButtonClicked(object o, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+    }
+}
